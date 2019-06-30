7a5d478993379ae1441e6e9c94383ceeb4577572
Felix <felix.benetzeder@blackrock.com>
2019-06-30 17:18:58 +0100

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(function (tutorial){
+    let str = tutorial.split(' ');
+  
+    let final = [ ];
+  
+     for(let word of str){
+       final.push(word.charAt(0).toUpperCase()+ word.slice(1));
+     }
+  
+   return final.join(' ') 
+  })
 }
