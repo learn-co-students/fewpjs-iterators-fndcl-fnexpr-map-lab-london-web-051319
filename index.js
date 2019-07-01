f8ba89bc92c8fc8edb6c52f4dc92ce9541baa4c7
ObaldThePedro <pedro.nunes.loureiro@gmail.com>
2019-07-01 11:59:08 +0100

    Done.

--- index.js
+++ index.js
@@ -13,10 +13,9 @@ const tutorials = [
 
 const titleCased = () => tutorials.map(function (tutorial) {
     tutorial = tutorial.split(' ');
-    console.log(tutorial);
     debugger;
     for (var i = 0; i < tutorial.length; i++) {
-    tutorial[i] = tutorial[i].charAt(0).toUpperCase();
+    tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
   }
   return tutorial.join(' ');
 });
